5cefb7f0fa042c7c82f7a4035f4669214b0af4cb
Mauricio Díaz <mauricio.diaz@brita.ai>
2023-07-09 22:20:42 -0600

    changes app

--- app.py
+++ app.py
@@ -11,15 +11,16 @@ CORS(app)
 def page_not_fount(error):
     return "<h1> 404 <h1>", 404
 
+app.config.from_object(config['development'])
+
+# Blueprints
+app.register_blueprint(MainRoute.main, url_prefix = '/')
+app.register_blueprint(AuthRoute.main, url_prefix = '/api/auth')
+app.register_blueprint(UserRoute.main, url_prefix = '/api/user')
+app.register_blueprint(TestRoute.main, url_prefix = '/api/access')
+
+# Error handlers
+app.register_error_handler(404, page_not_fount)
+
 if __name__ == '__main__':
-    app.config.from_object(config['development'])
-
-    # Blueprints
-    app.register_blueprint(MainRoute.main, url_prefix = '/')
-    app.register_blueprint(AuthRoute.main, url_prefix = '/api/auth')
-    app.register_blueprint(UserRoute.main, url_prefix = '/api/user')
-    app.register_blueprint(TestRoute.main, url_prefix = '/api/access')
-
-    # Error handlers
-    app.register_error_handler(404, page_not_fount)
     app.run()
