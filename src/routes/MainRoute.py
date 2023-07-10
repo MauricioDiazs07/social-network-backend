8a217547f3e7beb055fd5ff54033cd95b76e67c2
Mauricio Díaz <mauricio.diaz@brita.ai>
2023-07-10 14:06:47 -0600

    unecessary files

--- src/routes/MainRoute.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-main = Blueprint('login_blueprint', __name__)
-
-@main.route('/', methods = ['POST'])
-def main():
-    print("Todo funcionando")
-    return "Hola mundo de Influex"
