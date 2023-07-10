010f86500e5f16cc6c49b9c377b7990226bc05ae
Mauricio Díaz <mauricio.diaz@brita.ai>
2023-07-09 21:11:27 -0600

    unmake app function

--- app.py
+++ app.py
@@ -11,17 +11,14 @@ CORS(app)
 def page_not_fount(error):
     return "<h1> 404 <h1>", 404
 
-def create_app():
-        app.config.from_object(config['development'])
-
-        # Blueprints
-        app.register_blueprint(AuthRoute.main, url_prefix = '/api/auth')
-        app.register_blueprint(UserRoute.main, url_prefix = '/api/user')
-        app.register_blueprint(TestRoute.main, url_prefix = '/api/access')
-
-        # Error handlers
-        app.register_error_handler(404, page_not_fount)
-        app.run()
-
 if __name__ == '__main__':
-      create_app()
+    app.config.from_object(config['development'])
+
+    # Blueprints
+    app.register_blueprint(AuthRoute.main, url_prefix = '/api/auth')
+    app.register_blueprint(UserRoute.main, url_prefix = '/api/user')
+    app.register_blueprint(TestRoute.main, url_prefix = '/api/access')
+
+    # Error handlers
+    app.register_error_handler(404, page_not_fount)
+    app.run()
