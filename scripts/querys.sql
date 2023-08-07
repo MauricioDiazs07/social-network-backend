cc6517241fa62bb5576f793fbc7f3a61c805f2a7
Mauricio Díaz <mauricio.diaz@brita.ai>
2023-08-07 17:41:20 -0600

    update queries

--- scripts/querys.sql
+++ scripts/querys.sql
@@ -32,3 +32,60 @@ INSERT INTO "T_CATALOGUE_INTEREST" VALUES (25, 'Arte');
 INSERT INTO "T_CATALOGUE_INTEREST" VALUES (26, 'Motivación');
 INSERT INTO "T_CATALOGUE_INTEREST" VALUES (27, 'Viajes');
 INSERT INTO "T_CATALOGUE_INTEREST" VALUES (28, 'DIY');
+
+/* insert credentials values */
+-- user
+insert into "T_PROFILE" values (
+	'0c7ceb44a155db2fd60058e64eb255mk',
+	'user@brita.ai',
+	'Brita1234,',
+	'1',
+	'Alejandro Vázquez',
+	'H',
+	false,
+	'2023-07-31 19:20:06.548',
+	'https://cdn-icons-png.flaticon.com/512/3577/3577429.png',
+	''
+);
+
+insert into "T_USER_DATA" values (
+	1,
+	'0c7ceb44a155db2fd60058e64eb255mk',
+	'TAMAULIPAS',
+	'NUEVO LAREDO',
+	'C6 DE NOVIEMBRE 1920 COL CAMPESTRE 88278 NUEVO LAREDO, TAMPS.',
+	'1998-07-08',
+	'DISM980708MTSPRR08',
+	'https://cdn-icons-png.flaticon.com/512/3577/3577429.png',
+	false,
+	'',
+	''
+);
+
+-- admin
+insert into "T_PROFILE" values (
+	'0c7ceb44a155db2fd60058e64eb255ch',
+	'admin@brita.ai',
+	'Brita1234,',
+	'2',
+	'Victoria Hernández',
+	'M',
+	false,
+	'2023-07-31 19:20:06.548',
+	'https://aishlatino.com/wp-content/uploads/2021/11/que-tipo-de-persona-te-gustaria-ser-730x411-SP.jpg',
+	''
+);
+
+insert into "T_USER_DATA" values (
+	2,
+	'0c7ceb44a155db2fd60058e64eb255ch',
+	'TAMAULIPAS',
+	'NUEVO LAREDO',
+	'C6 DE NOVIEMBRE 1920 COL CAMPESTRE 88278 NUEVO LAREDO, TAMPS.',
+	'1999-05-08',
+	'DIMS990701MTSPRR05',
+	'https://aishlatino.com/wp-content/uploads/2021/11/que-tipo-de-persona-te-gustaria-ser-730x411-SP.jpg',
+	false,
+	'',
+	''
+);
