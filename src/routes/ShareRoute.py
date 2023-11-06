11fd0602acdbdbbcaee0459b6b7a840bb289e898
Jonathan-YV <yair.vazquez001@gmail.com>
2023-11-05 21:29:59 -0600

    Share from profile

--- src/routes/ShareRoute.py
+++ src/routes/ShareRoute.py
@@ -190,33 +190,34 @@ def list_share_from_profile(profile_id):
         likes = InteractionModel.get_all_likes()
         post = []
         for share in shares:
-            post_multimedia = []
-            post_comment = []
-            post_like = []
-            autoLike = False
-            for multimedia in multimedias:
-                if 'share_id' in multimedia and share['id'] == multimedia['share_id']:
-                    multimedia.pop('share_id')
-                    multimedia.pop('share_type')                      
-                    post_multimedia.append(multimedia)
-            for comment in comments:
-                if 'share_id' in comment and share['id'] == comment['share_id']:
-                    comment.pop('share_id')
-                    comment.pop('share_type')
-                    post_comment.append(comment)
-            for like in likes:
-                print(like)
-                if 'share_id' in like and share['id'] == like['share_id']:
-                    like.pop('share_id')
-                    like.pop('share_type')
-                    post_like.append(like)
-                    if like['profile_id'] == share['profileId']:
-                        autoLike = True
-            share['multimedia'] = {"count": len(post_multimedia), "data": post_multimedia}
-            share['comments'] = {"count": len(post_comment), "data": post_comment}
-            share['likes'] = {"count": len(post_like), "data": post_like, "like": autoLike}
-            share['creationDate'] = reformatCreatedDate(share['creationDate'])
-            post.append(share)
+            if share['shareType'] == 'POST':
+                post_multimedia = []
+                post_comment = []
+                post_like = []
+                autoLike = False
+                for multimedia in multimedias:
+                    if 'share_id' in multimedia and share['id'] == multimedia['share_id']:
+                        multimedia.pop('share_id')
+                        multimedia.pop('share_type')                      
+                        post_multimedia.append(multimedia)
+                for comment in comments:
+                    if 'share_id' in comment and share['id'] == comment['share_id']:
+                        comment.pop('share_id')
+                        comment.pop('share_type')
+                        post_comment.append(comment)
+                for like in likes:
+                    print(like)
+                    if 'share_id' in like and share['id'] == like['share_id']:
+                        like.pop('share_id')
+                        like.pop('share_type')
+                        post_like.append(like)
+                        if like['profile_id'] == share['profileId']:
+                            autoLike = True
+                share['multimedia'] = {"count": len(post_multimedia), "data": post_multimedia}
+                share['comments'] = {"count": len(post_comment), "data": post_comment}
+                share['likes'] = {"count": len(post_like), "data": post_like, "like": autoLike}
+                share['creationDate'] = reformatCreatedDate(share['creationDate'])
+                post.append(share)
         return post
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
